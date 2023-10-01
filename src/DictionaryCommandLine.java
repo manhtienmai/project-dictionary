8c4e6becf891b1864233ae02f74fbf4f92f1a307
Nguyen Quoc Vuong <nguyenquocvuong161104@gmail.com>
2023-10-01 20:15:25 +0700

    first commit

--- src/DictionaryCommandLine.java
+++ src/DictionaryCommandLine.java
@@ -31,23 +31,57 @@ public class DictionaryCommandLine {
                     System.out.print("Goodbye!");
                     return;
                 case "1":
-                    return;
+                    System.out.println("Enter English word:");
+                    String eng = sc.nextLine();
+                    System.out.print("Enter Vietnamese meaning:");
+                    String vie = sc.nextLine();
+                    dictionaryManagement.addWord(new Word(eng, vie));
+                    System.out.println("Word added!");
+                    break;
                 case "2":
-                    return;
+                    System.out.println("Enter English word to remove:");
+                    String wordToRemove = sc.nextLine();
+                    dictionaryManagement.deleteWord(wordToRemove);
+                    System.out.println("Successfully delete word");
+                    break;
                 case "3":
-                    return;
+                    System.out.println("Enter English word to update:");
+                    String presentWord = sc.nextLine();
+                    System.out.println("Enter new English word:");
+                    String newWord = sc.nextLine();
+                    System.out.println("Enter new Vietnamese meaning:");
+                    String newMeaning = sc.nextLine();
+                    dictionaryManagement.updateWord(presentWord, newWord, newMeaning);
+                    System.out.println("Successfully update word");
+                    break;
                 case "4":
                     showAllWords();
                     break;
                 case "5":
+                    System.out.println("Enter English word to lookup:");
+                    String wordToLookup = sc.nextLine();
+                    Word result = dictionaryManagement.dictionarySearch(wordToLookup);
+                    if (result != null) {
+                        System.out.println(wordToLookup + ": " + result.getWord_explain());
+                    } else {
+                        System.out.println("Not found word.");
+                    }
                     break;
                 case "6":
+                    System.out.println("Enter prefix to search:");
+                    String prefix = sc.nextLine();
+                    System.out.println(dictionaryManagement.dictionarySearch(prefix));
                     break;
                 case "7":
+                    System.out.println("To be continue");
                     break;
                 case "8":
+                    dictionaryManagement.insertFromFile();
+                    System.out.println("Imported successfully");
                     break;
                 case "9":
+                    dictionaryManagement.dictionaryExportToFile();
+                    System.out.println("Exported successfully");
                     break;
                 default:
                     System.out.print("Action not supported");
--- src/DictionaryManagement.java
+++ src/DictionaryManagement.java
@@ -1,8 +1,20 @@
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Dictionary;
 import java.util.Scanner;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
 
 public class DictionaryManagement {
     private Dictionary dictionary;
-
+    private ArrayList<Word> library = new ArrayList<Word>();
+    String path = "dictionaries.txt";
     public DictionaryManagement(Dictionary dictionary) {
         this.dictionary = dictionary;
     }
@@ -22,10 +34,73 @@ public class DictionaryManagement {
         }
     }
 
-    public void insertFromFile() {}
-    public void addWord(Word word) {}
-    public Word dictionarySearch(String target) {}
-    public void editWord() {}
-    public void deleteWord(String target) {}
-    public void dictionaryExportToFile() {}
+    public void insertFromFile() {
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            String line;
+            while((line = reader.readLine()) != null) {
+                String[] data = line.split("\t");
+                if (data.length == 2) {
+                    String eng = data[0].trim();
+                    String vie = data[1].trim();
+                    dictionary.addWord(new Word(eng, vie));
+                }
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void addWord(Word word) {
+        library.add(new Word(word.getWord_target(), word.getWord_explain()));
+    }
+
+    public String getWords(){
+        return library;
+    }
+    public String dictionarySearch(String target) {
+        for (String[] entry : library) {
+            if (entry[0].equals(target)) {
+                return entry[1];
+            } else if (entry[1].equals(target)) {
+                return entry[0];
+            }
+        }
+        String s = "Can't find this word.";
+        return  s;
+    }
+    public void editWord(String tagert) {
+        int pos = 0;
+        for (int i = 0; i < library.size(); i++) {
+            if (library.get(i).getWord_target().equals(tagert)) {
+                pos = i;
+                break;
+            }
+        }
+        if (library.get(pos).getWord_target().equals(tagert)) {
+            library.get(pos).setWord_explain(tagert);
+        }
+    }
+    public void deleteWord(String target) {
+        for (int i = 0; i < library.size(); i++) {
+            String[] entry = library.get(i);
+            if (entry[0].equals(target) || entry[1].equals(target)) {
+                library.remove(i);
+                break;
+            }
+        }
+    }
+    public void dictionaryExportToFile() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(path))) {
+            for (String[] entry : library) {
+                String word = entry[0];
+                String meaning = entry[1];
+                String line = word + "\t" + meaning;
+                writer.write(line);
+                writer.newLine(); // Thêm dấu xuống dòng sau mỗi cặp từ và nghĩa
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
